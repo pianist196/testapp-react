9d25acdf7b7cbe5476eac27f5cb4f12083dcf52d
pianist196 <1234569050@rambler.ru>
2021-03-28 22:37:05 +0500

    axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Home from './pages/Home'
 import Filters from './pages/Filters'
 import { Route } from 'react-router';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+
+	const [hotels, setHotels] = useState([])
+
+	useEffect(() => {
+		axios.get('http://localhost:3000/db.json').then(({ data }) => {
+			setHotels(data.hotels)
+		})
+	}, [])
+
 	return (
 		<>
-			<Route exact path="/" component={Home} />
+			<Route exact path="/" render={() => <Home items={hotels} />} />
 			<Route exact path="/filters" component={Filters} />
 		</>
 	);
---
+++ src/HotelCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function HotelCard({ name, address, imageURL, distance, price }) {
+	return (
+		<div className="main__card">
+			<div className="main__card-img">
+				<img src={imageURL} alt="" />
+			</div>
+			<div className="main__card-content">
+				<div className="main__card-title">{name}</div>
+				<div className="main__card-address">{address}</div>
+				<div className="main__card-distance">{distance}</div>
+				<div className="main__card-price">
+					{price}
+					<span>за ночь</span>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default HotelCard
